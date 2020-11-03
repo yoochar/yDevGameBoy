0ae2509ff56bddef106309c7a096ca4c583ff25d
yoochar <yoochar@gmail.com>
2020-11-04 00:43:50 +0700

    first

---
+++ yGB101/main.c
@@ -0,0 +1,19 @@
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main(){
+
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \n");
+    printf(" \    ----------\n");
+    printf(" \n");
+    printf("    HELLO WORLD\n");
+    printf(" \n");
+    printf(" \n");
+    printf(" by Prolang Nazorn");
+    printf(" \n");
+    printf(" \n");
+    printf(" \    ----------\n");
+}
